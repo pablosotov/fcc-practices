6c082538d12a96c9d5fc3fa0b0aa7f8f3565de9f
Pablo Soto <jpablo33@gmail.com>
2017-05-02 14:14:51 -0600

    Update weather-api.js

--- js/weather-api.js
+++ js/weather-api.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
   myLocation();  //create function to identify your location city and country
   function myLocation(){
-    var coordenates, lat, lon;
+    var coordenates; 
     var myCountry;
     $.get(
       "https://ipinfo.io",
@@ -11,16 +11,15 @@ $(document).ready(function() {
 
         //console.log(location);
         coordenates = location.loc.split(",");
-        lat = coordenates[0];
-        lon = coordenates[1];
+        var lat = coordenates[0];
+        var lon = coordenates[1];
         myWeather(lat, lon);
         myCountry = location.country;
         $("#location").html(location.city+", "+location.region); 
-
+        
         // Countries that don't use metric system
         //  MM: "Myanmar", LR: "Liberia",  US: "United States"  
         countryNames();
-        
         //convert the signature of your country to the full name
         function countryNames(){
           $.get(
@@ -31,35 +30,54 @@ $(document).ready(function() {
               for(var i = 0; i < qty; i++){
                 if (Object.keys(Countries)[i] == myCountry){
                   fullname = Countries[Object.keys(Countries)[i]];
-                  console.log(fullname);
+                  //console.log(fullname);
                 }
               }
              $("#country").html(fullname);
             }, "json"
-            
-          );
+          );  // end get country names
         }    // end function countryNames   
+       
       }, "jsonp"
-    );
-  }
-
-
-
+      
+    );  // end get ipinfo
+     
+  } // end function myLocation
   
-  ////////////////////////
-  
-
-
+  var weatherjson;
   function myWeather(lat, lon){
     // get the weather json according to your location
-    var weatherjson = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=metric&appid=e3685cd80af250dff079b263ff03b45f";
-    //console.log(weatherjson);
+    weatherjson = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=metric&appid=e3685cd80af250dff079b263ff03b45f";
     
-    $.get(weatherjson, function(weather) {
-      //console.log(weather);
-      $("#temperature").html(weather.main.temp+" ºC");
-    }, "jsonp");
-    
-  }  
+    $.get(
+        weatherjson,
+        function(theWeather) {
+          console.log(theWeather);
+          $("#temperature").html(theWeather.main.temp+" ºC");
+
+          var prefix = 'wi wi-';
+          var code = theWeather.weather[0].id;
+          console.log(code);
+          var icon = theWeather.weather[code].icon;
+          console.log(theWeather.weather[0].id);
+          
+          // If we are not in the ranges mentioned above, add a day/night prefix.
+          if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
+            icon = 'day-' + icon;
+          }
+
+          // Finally tack on the prefix.
+          icon = prefix + icon;
+          console.log(icon);
+          
+        }, "jsonp"
+    );
+
+  }  // end function myWeather
+
+
+
+
+  
 }); // end document.ready
 
